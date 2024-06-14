e923b3ae40ed0aa30fabebfd4d8dc335c1b170a3
Sinan <sinan.ks786@gmail.com>
2024-06-14 21:34:21 +0530

    Added a popup messages box

--- StudentHub/views.py
+++ StudentHub/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
-
 from django.views.generic import View
-
+from django.contrib import messages
 from StudentHub.models import Category, Student
-
 from StudentHub.forms import CategoryForm, StudentForm
 
 # Create your views here.
@@ -13,19 +11,15 @@ class Home(View):
     def get(self, request, *args, **kwargs):
 
         cat_form = CategoryForm()
-        
         categories = Category.objects.all()
-
         students = Student.objects.all()
 
-        return render(request, "index.html", {"categories": categories, "students":  students, 'cat_form': cat_form})
+        return render(request, "index.html", {"categories": categories, "students": students, 'cat_form': cat_form})
 
     def post(self, request, *args, **kwargs):
 
         cat_form = CategoryForm()
-
         categories = Category.objects.all()
-
         students = Student.objects.all()
 
         if "category_name" in request.POST:
@@ -33,29 +27,21 @@ class Home(View):
             cat_form = CategoryForm(request.POST)
             
             if cat_form.is_valid():
-
                 cat_form.save()
-
+                messages.success(request, "New category added successfully.")
                 return redirect('home')
-
+            
         if "category" in request.POST:
 
             if request.POST.get('category') == "all":
-
                 students = Student.objects.all()
-
                 return redirect('home')
-
+            
             category_id = request.POST.get("category")
-
             category_obj = Category.objects.get(id=category_id)
-            
-            
             students = Student.objects.filter(category_name=category_obj)
 
-
-        return render(request, "index.html", {"categories": categories, "students":  students, 'cat_form':cat_form})
-    
+        return render(request, "index.html", {"categories": categories, "students": students, 'cat_form': cat_form})
 
 class CatFormView(View):
 
@@ -65,95 +51,73 @@ class CatFormView(View):
 
         return render(request, "cat_form.html", {"cat_form": cat_form})
 
-
     def post(self, request, *args, **kwargs):
 
         cat_form = CategoryForm(request.POST)
 
         if cat_form.is_valid():
-
             cat_form.save()
-
+            messages.success(request, "New category added successfully.")
             return redirect("home")
         
         return render(request, "cat_form.html", {"cat_form": cat_form})
-    
 
 class StuFormView(View):
 
     def get(self, request, *args, **kwargs):
 
         stu_form = StudentForm()
-
         return render(request, "stu_form.html", {"stu_form": stu_form})
-
 
     def post(self, request, *args, **kwargs):
 
         stu_form = StudentForm(request.POST)
-        
+
         if stu_form.is_valid():
-
             stu_form.save()
-
+            messages.success(request, "New student added successfully.")
             return redirect("home")
-    
-        return render(request, "stu_form.html", { "stu_form": stu_form})
-    
-
-# Fetching a specific employee
+        
+        return render(request, "stu_form.html", {"stu_form": stu_form})
 
 class StudentDetailView(View):
 
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
 
-        id=kwargs.get('pk')
+        id = kwargs.get('pk')
+        qs = Student.objects.get(id=id)
 
-        qs=Student.objects.get(id=id)
-
-        return render(request,'student_detail.html',{'data':qs})
-    
-# Remove a specific employee
+        return render(request, 'student_detail.html', {'data': qs})
 
 class StudentDeleteView(View):
 
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
 
-        id=kwargs.get('pk')
-
+        id = kwargs.get('pk')
         Student.objects.get(id=id).delete()
+        messages.success(request, "Student deleted successfully.")
 
         return redirect('home')
-    
 
-# Updating Student
-    
 class StudentUpdateView(View):
 
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
 
-        id=kwargs.get('pk')
+        id = kwargs.get('pk')
+        student_object = Student.objects.get(id=id)
+        form = StudentForm(instance=student_object)
 
-        student_object=Student.objects.get(id=id)
+        return render(request, 'student_edit.html', {'form': form})
 
-        form=StudentForm(instance=student_object)
+    def post(self, request, *args, **kwargs):
 
-        return render(request,'student_edit.html',{'form':form})
-    
-    def post(self,request,*args,**kwargs):
-
-        id=kwargs.get('pk')
-
-        student_object=Student.objects.get(id=id)
-
-        form=StudentForm(request.POST,instance=student_object)
+        id = kwargs.get('pk')
+        student_object = Student.objects.get(id=id)
+        form = StudentForm(request.POST, instance=student_object)
 
         if form.is_valid():
-
             form.save()
-
+            messages.success(request, "Student details updated successfully.")
             return redirect('home')
-        
         else:
-            
-            return render(request,'student_edit.html',{'form':form})
+            return render(request, 'student_edit.html', {'form': form})
